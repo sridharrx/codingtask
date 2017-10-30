53304fa6852066b322b18e0abfd067e8d1bfa9fa
sridharrx <33230814+sridharrx@users.noreply.github.com>
2017-10-30 22:31:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+   function GitHubCtrl($scope, $http) {
+      $scope.userNotFound = false;
+      $scope.loaded = false;
+      $scope.getGitInfo = function () {
+         $http.get("https://api.github.com/users/" + $scope.username)
+               .success(function (data) {
+                  if (data.name == "") data.name = data.login;
+                  $scope.user = data;
+                  $scope.loaded = true;
+               })
+               .error(function () {
+                  $scope.userNotFound = true;
+               });
+         $http.get("https://api.github.com/users/" + $scope.username + "/repos").success(function (data) {
+            $scope.repos = data;
+            $scope.reposFound = data.length > 0;
+         });
+      }
+   }
