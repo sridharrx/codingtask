3401fbcc4617ed3314633db8e1e2e441abd0e5de
gamezguru <gamezguru@gmail.com>
2017-12-16 17:31:05 +0530

    modify from local 1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
                })
                .error(function () {
                   $scope.userNotFound = true;
-            //added one line 
+            //local modify one
                });
          $http.get("https://api.github.com/users/" + $scope.username + "/repos").success(function (data) {
             $scope.repos = data;
