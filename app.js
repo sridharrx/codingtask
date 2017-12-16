89470137f6019b612b54d0542ceb433302a4200a
sridharrx <33230814+sridharrx@users.noreply.github.com>
2017-12-16 17:32:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
                })
                .error(function () {
                   $scope.userNotFound = true;
-            //local modify one
+            //server modify one
                });
          $http.get("https://api.github.com/users/" + $scope.username + "/repos").success(function (data) {
             $scope.repos = data;
